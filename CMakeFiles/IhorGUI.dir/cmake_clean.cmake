e8cdf56627d9fabcca83ce1642a0862adbdfe526
Ihor Vasylets <ihorvasyletswrk@gmail.com>
2023-02-03 21:44:46 +0200

    initial commit

---
+++ CMakeFiles/IhorGUI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IhorGUI.dir/src/IhorGUI.cpp.o"
+  "libIhorGUI.a"
+  "libIhorGUI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IhorGUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
